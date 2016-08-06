8df57a51aee4f5feda38d6e37bd9b2f6defb67a3
qiyi <bphanzhu@gmail.com>
2016-08-06 11:39:36 +0800

    add string slice funcs

---
+++ slices/slices.go
@@ -0,0 +1,35 @@
+package slices
+
+// IndexStr 从一个 []string 里查询字符串并返回所在下标
+func IndexStr(slice []string, str string) int {
+    if len(slice) != 0 {
+        for i, s := range slice {
+            if s == str {
+                return i
+            }
+        }
+    }
+    return -1
+}
+
+// ContainsStr 测试一个 []string 里是否包含某字符串
+func ContainsStr(slice []string, str string) bool {
+    return IndexStr(slice, str) != -1
+}
+
+// DeleteStr 从 []string 里移除某个字符串
+func DeleteStr(slice []string, str string) ([]string, bool) {
+    idx := IndexStr(slice, str)
+    if idx != -1 {
+        return append(slice[:idx], slice[idx + 1:]...), true
+    }
+    return slice, false
+}
+
+// AddFirstStr 将一个字符串添加到 []string 的头部
+func AddFirstStr(slice []string, str string) []string {
+    return append([]string{str}, slice...)
+}
+
+
+
---
+++ slices/slices_test.go
@@ -0,0 +1,73 @@
+package slices
+
+import "testing"
+
+func TestNotContainsStr(t *testing.T) {
+    slice := []string{"a", "b", "c"}
+    if !ContainsStr(slice, "b") {
+        t.Fail()
+    }
+}
+
+func TestContainsStr(t *testing.T) {
+    slice := []string{"a", "b", "c"}
+    if ContainsStr(slice, "d") {
+        t.Fail()
+    }
+}
+
+func TestIndexStrOK(t *testing.T) {
+    slice := []string{"a", "b", "c"}
+    if IndexStr(slice, "b") != 1 {
+        t.Fail()
+    }
+}
+
+func TestIndexStrNotExist(t *testing.T) {
+    slice := []string{"a", "b", "c"}
+    if IndexStr(slice, "d") != -1 {
+        t.Fail()
+    }
+}
+
+func TestDeleteNilStrSlice(t *testing.T) {
+    var slice []string
+    if _, ok := DeleteStr(slice, "a"); ok {
+        t.Fail()
+    }
+}
+
+func TestDeleteEmptyStrSlice(t *testing.T) {
+    slice := []string{}
+    if _, ok := DeleteStr(slice, "a"); ok {
+        t.Fail()
+    }
+}
+
+func TestDeleteOneStrSlice(t *testing.T) {
+    slice := []string{"a"}
+    if result, ok := DeleteStr(slice, "a"); !ok {
+        t.Fail()
+    } else {
+        if len(result) != 0 {
+            t.Fail()
+        }
+    }
+}
+
+func TestAddFirstStrNilSlice(t *testing.T) {
+    var slice []string
+    result := AddFirstStr(slice, "a")
+    if len(result) != 1 {
+        t.Fail()
+    }
+}
+
+func TestAddFirstStr(t *testing.T) {
+    slice := []string{"b"}
+    result := AddFirstStr(slice, "a")
+    if result[0] != "a" {
+        t.Fail()
+    }
+}
+
