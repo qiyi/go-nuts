d7ab1b83cbca7cab9f181ef20861b6ad8edc35ce
qiyi <bphanzhu@gmail.com>
2016-08-28 11:42:18 +0800

    fix bug when delete slice

--- slices/slices.go
+++ slices/slices.go
@@ -2,71 +2,69 @@ package slices
 
 // IndexStr 从一个 []string 里查询字符串并返回所在下标
 func IndexStr(slice []string, str string) int {
-    if len(slice) != 0 {
-        for i, s := range slice {
-            if s == str {
-                return i
-            }
-        }
-    }
-    return -1
+	if len(slice) != 0 {
+		for i, s := range slice {
+			if s == str {
+				return i
+			}
+		}
+	}
+	return -1
 }
 
 // ContainsStr 测试一个 []string 里是否包含某字符串
 func ContainsStr(slice []string, str string) bool {
-    return IndexStr(slice, str) != -1
+	return IndexStr(slice, str) != -1
 }
 
 // DeleteStrAt 根据下标从 []string 里删除字符串
 func DeleteStrAt(slice []string, idx int) ([]string, bool) {
-    if idx >= 0 && idx < len(slice) {
-        return append(slice[:idx], slice[idx + 1:]...), true
-    }
-    return slice, false
+	if idx >= 0 && idx < len(slice) {
+		result := append([]string{}, slice[:idx]...)
+		result = append(result, slice[idx+1:]...)
+		return result, true
+	}
+	return slice, false
 }
 
 // DeleteStr 从 []string 里移除某个字符串
 func DeleteStr(slice []string, str string) ([]string, bool) {
-    idx := IndexStr(slice, str)
-    if idx != -1 {
-        return DeleteStrAt(slice, idx)
-    }
-    return slice, false
+	idx := IndexStr(slice, str)
+	if idx != -1 {
+		return DeleteStrAt(slice, idx)
+	}
+	return slice, false
 }
 
 // AddFirstStr 将一个字符串添加到 []string 的头部
 func AddFirstStr(slice []string, str string) []string {
-    return append([]string{str}, slice...)
+	return append([]string{str}, slice...)
 }
 
 // InsertStr 插入一个字符串到指定下标位置
 func InsertStr(slice []string, at int, str string) ([]string, bool) {
-    if at == len(slice) {
-        return append(slice, str), true
-    }
-    if at >= 0 && at < len(slice) {
-        result := append([]string{}, slice[:at]...)
-        result = append(result, str)
-        result = append(result, slice[at:]...)
-        return result, true
-    }
-    return slice, false
+	if at == len(slice) {
+		return append(slice, str), true
+	}
+	if at >= 0 && at < len(slice) {
+		result := append([]string{}, slice[:at]...)
+		result = append(result, str)
+		result = append(result, slice[at:]...)
+		return result, true
+	}
+	return slice, false
 }
 
 // ForeachStr 迭代 []string 执行方法
 func ForeachStr(slice []string, f func(int, string)) {
-    if slice != nil {
-        for i, v := range slice {
-            f(i, v)
-        }
-    }
+	if slice != nil {
+		for i, v := range slice {
+			f(i, v)
+		}
+	}
 }
 
 // NewStrSlice 创建一个初始长度为 0 的 []string
 func NewStrSlice() []string {
-    return make([]string, 0)
+	return make([]string, 0)
 }
-
-
-
-
