4a1848357f3541890a8912d68117d34d59447078
qiyi <bphanzhu@gmail.com>
2016-10-15 18:33:17 +0800

    Add visit serial methods for maps

--- maps/maps.go
+++ maps/maps.go
@@ -1,7 +1,13 @@
 package maps
 
+import (
+	"errors"
+	"github.com/qiyi/go-nuts/slices"
+	"strings"
+)
+
 // Get 从 map[interface{}]interface{} 里获取值
-func Get(m map[interface{}]interface{}, key string) (interface{}, bool) {
+func Get(m map[interface{}]interface{}, key interface{}) (interface{}, bool) {
 	if m != nil {
 		if value, ok := m[key]; ok {
 			return value, ok
@@ -11,7 +17,7 @@ func Get(m map[interface{}]interface{}, key string) (interface{}, bool) {
 }
 
 // GetStr 从 map[interface{}]interface{} 里获取 string 值
-func GetStr(m map[interface{}]interface{}, key string) (string, bool) {
+func GetStr(m map[interface{}]interface{}, key interface{}) (string, bool) {
 	if value, ok := Get(m, key); ok {
 		if result, ok := value.(string); ok {
 			return result, ok
@@ -21,7 +27,7 @@ func GetStr(m map[interface{}]interface{}, key string) (string, bool) {
 }
 
 // GetInt 从 map[interface{}]interface{} 里获取 int 值
-func GetInt(m map[interface{}]interface{}, key string) (int, bool) {
+func GetInt(m map[interface{}]interface{}, key interface{}) (int, bool) {
 	if value, ok := Get(m, key); ok {
 		if result, ok := value.(int); ok {
 			return result, ok
@@ -31,7 +37,7 @@ func GetInt(m map[interface{}]interface{}, key string) (int, bool) {
 }
 
 // GetBool 从 map[interface{}]interface{} 里获取 bool 值
-func GetBool(m map[interface{}]interface{}, key string) (bool, bool) {
+func GetBool(m map[interface{}]interface{}, key interface{}) (bool, bool) {
 	if value, ok := Get(m, key); ok {
 		if result, ok := value.(bool); ok {
 			return result, ok
@@ -41,7 +47,7 @@ func GetBool(m map[interface{}]interface{}, key string) (bool, bool) {
 }
 
 // GetSlice 从 map[interface{}]interface{} 里获取 []interface{} 值
-func GetSlice(m map[interface{}]interface{}, key string) ([]interface{}, bool) {
+func GetSlice(m map[interface{}]interface{}, key interface{}) ([]interface{}, bool) {
 	if value, ok := Get(m, key); ok {
 		if result, ok := value.([]interface{}); ok {
 			return result, ok
@@ -51,7 +57,7 @@ func GetSlice(m map[interface{}]interface{}, key string) ([]interface{}, bool) {
 }
 
 // GetStrSlice 从 map[interface{}]interface{} 里获取 []string 值
-func GetStrSlice(m map[interface{}]interface{}, key string) ([]string, bool) {
+func GetStrSlice(m map[interface{}]interface{}, key interface{}) ([]string, bool) {
 	if value, ok := Get(m, key); ok {
 		if result, ok := value.([]string); ok {
 			return result, true
@@ -69,8 +75,28 @@ func GetStrSlice(m map[interface{}]interface{}, key string) ([]string, bool) {
 	return nil, false
 }
 
+// GetMap 从 map[interface{}]interface{} 里获取 map[interface{}]interface{} 值
+func GetMap(m map[interface{}]interface{}, key interface{}) (map[interface{}]interface{}, bool) {
+	if value, ok := Get(m, key); ok {
+		if result, ok := value.(map[interface{}]interface{}); ok {
+			return result, ok
+		}
+	}
+	return nil, false
+}
+
+// GetMap 从 map[interface{}]interface{} 里获取 map[string]interface{} 值
+func GetStrMap(m map[interface{}]interface{}, key interface{}) (map[string]interface{}, bool) {
+	if value, ok := Get(m, key); ok {
+		if result, ok := value.(map[string]interface{}); ok {
+			return result, ok
+		}
+	}
+	return nil, false
+}
+
 // GetMapSlice 从 map[interface{}]interface{} 里获取 []map[interface{}]interface{} 值
-func GetMapSlice(m map[interface{}]interface{}, key string) ([]map[interface{}]interface{}, bool) {
+func GetMapSlice(m map[interface{}]interface{}, key interface{}) ([]map[interface{}]interface{}, bool) {
 	if value, ok := Get(m, key); ok {
 		if result, ok := value.([]map[interface{}]interface{}); ok {
 			return result, true
@@ -84,28 +110,9 @@ func GetMapSlice(m map[interface{}]interface{}, key string) ([]map[interface{}]i
 			}
 		}
 		return result, true
+	} else {
+		return nil, false
 	}
-	return nil, false
-}
-
-// GetMap 从 map[interface{}]interface{} 里获取 map[interface{}]interface{} 值
-func GetMap(m map[interface{}]interface{}, key string) (map[interface{}]interface{}, bool) {
-	if value, ok := Get(m, key); ok {
-		if result, ok := value.(map[interface{}]interface{}); ok {
-			return result, ok
-		}
-	}
-	return nil, false
-}
-
-// GetMap 从 map[interface{}]interface{} 里获取 map[string]interface{} 值
-func GetStrMap(m map[interface{}]interface{}, key string) (map[string]interface{}, bool) {
-	if value, ok := Get(m, key); ok {
-		if result, ok := value.(map[string]interface{}); ok {
-			return result, ok
-		}
-	}
-	return nil, false
 }
 
 // CloneStrMap 克隆 map[interface{}]interface{} 到一个新的 map[interface{}]interface
@@ -157,4 +164,212 @@ func NewMap() map[interface{}]interface{} {
 // NewStrMap 创建 map[string]interface{}
 func NewStrMap() map[string]interface{} {
 	return make(map[string]interface{})
+}
+
+// Visit 根据 path 访问 map[interface{}]interface 获取 interface{} 值
+func VisitValue(m map[interface{}]interface{}, paths []interface{}) (interface{}, error) {
+	if m == nil {
+		return nil, errors.New("Nil value")
+	}
+
+	if paths == nil {
+		return m, nil
+	}
+
+	var node interface{} = m
+	visited := make([]interface{}, 0)
+	for _, path := range paths {
+		visited = append(visited, path)
+		switch n := node.(type) {
+		case map[interface{}]interface{}:
+			if result, ok := Get(n, path); ok {
+				node = result
+			} else {
+				return nil, errors.New("Visit faild:" + strings.Join(slices.AsStrSlice(visited), ","))
+			}
+		case map[string]interface{}:
+			if result, ok := Get(AsMap(n), path); ok {
+				node = result
+			} else {
+				return nil, errors.New("Visit faild:" + strings.Join(slices.AsStrSlice(visited), ","))
+			}
+		case []map[interface{}]interface{}:
+			if idx, ok := path.(int); ok {
+				if len(n) > idx {
+					node = n[idx]
+					continue
+				}
+			}
+			return nil, errors.New("Visit faild:" + strings.Join(slices.AsStrSlice(visited), ","))
+		case []map[string]interface{}:
+			if idx, ok := path.(int); ok {
+				if len(n) > idx {
+					node = n[idx]
+					continue
+				}
+			}
+			return nil, errors.New("Visit faild:" + strings.Join(slices.AsStrSlice(visited), ","))
+		case []interface{}:
+			if idx, ok := path.(int); ok {
+				if len(n) > idx {
+					node = n[idx]
+					continue
+				}
+			}
+			return nil, errors.New("Visit faild:" + strings.Join(slices.AsStrSlice(visited), ","))
+		case []string:
+			if idx, ok := path.(int); ok {
+				if len(n) > idx {
+					node = n[idx]
+					continue
+				}
+			}
+			return nil, errors.New("Visit faild:" + strings.Join(slices.AsStrSlice(visited), ","))
+		default:
+			return nil, errors.New("Visit faild:" + strings.Join(slices.AsStrSlice(visited), ","))
+		}
+	}
+	return node, nil
+}
+
+// Visit 根据 path 访问 map[interface{}]interface 获取 interface{} 值
+func Visit(m map[interface{}]interface{}, paths ...interface{}) (interface{}, error) {
+	return VisitValue(m, paths)
+}
+
+// VisitStr 根据 path 访问 map[interface{}]interface 获取 string 值
+func VisitStr(m map[interface{}]interface{}, paths ...interface{}) (string, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.(string); ok {
+			return result, nil
+		} else {
+			return "", errors.New("Type assert error.")
+		}
+	} else {
+		return "", err
+	}
+}
+
+// VisitInt 根据 path 访问 map[interface{}]interface 获取 int 值
+func VisitInt(m map[interface{}]interface{}, paths ...interface{}) (int, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.(int); ok {
+			return result, nil
+		} else {
+			return 0, errors.New("Type assert error.")
+		}
+	} else {
+		return 0, err
+	}
+}
+
+// VisitBool 根据 path 访问 map[interface{}]interface 获取 bool 值
+func VisitBool(m map[interface{}]interface{}, paths ...interface{}) (bool, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.(bool); ok {
+			return result, nil
+		} else {
+			return false, errors.New("Type assert error.")
+		}
+	} else {
+		return false, err
+	}
+}
+
+// VisitSlice 根据 path 访问 map[interface{}]interface 获取 []interface{} 值
+func VisitSlice(m map[interface{}]interface{}, paths ...interface{}) ([]interface{}, error) {
+	return visitSliceValue(m, paths)
+}
+
+func visitSliceValue(m map[interface{}]interface{}, paths []interface{}) ([]interface{}, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.([]interface{}); ok {
+			return result, nil
+		} else {
+			return nil, errors.New("Type assert error.")
+		}
+	} else {
+		return nil, err
+	}
+}
+
+// VisitStrSlice 根据 path 访问 map[interface{}]interface 获取 []string 值
+func VisitStrSlice(m map[interface{}]interface{}, paths ...interface{}) ([]string, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.([]string); ok {
+			return result, nil
+		} else {
+			return nil, errors.New("Type assert error.")
+		}
+	} else {
+		return nil, err
+	}
+}
+
+// VisitMap 根据 path 访问 map[interface{}]interface 获取 map[interface{}]interface{} 值
+func VisitMap(m map[interface{}]interface{}, paths ...interface{}) (map[interface{}]interface{}, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.(map[interface{}]interface{}); ok {
+			return result, nil
+		} else {
+			return nil, errors.New("Type assert error.")
+		}
+	} else {
+		return nil, err
+	}
+}
+
+// VisitStrMap 根据 path 访问 map[interface{}]interface 获取 map[string]interface{} 值
+func VisitStrMap(m map[interface{}]interface{}, paths ...interface{}) (map[string]interface{}, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.(map[string]interface{}); ok {
+			return result, nil
+		} else {
+			return nil, errors.New("Type assert error.")
+		}
+	} else {
+		return nil, err
+	}
+}
+
+// VisitMapSlice 根据 path 访问 map[interface{}]interface{} 获取 []map[interface{}]interface{} 值
+func VisitMapSlice(m map[interface{}]interface{}, paths ...interface{}) ([]map[interface{}]interface{}, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.([]map[interface{}]interface{}); ok {
+			return result, nil
+		}
+	}
+
+	if slice, err := visitSliceValue(m, paths); err == nil {
+		result := make([]map[interface{}]interface{}, 0)
+		for _, s := range slice {
+			if dic, ok := s.(map[interface{}]interface{}); ok {
+				result = append(result, dic)
+			}
+		}
+		return result, nil
+	} else {
+		return nil, err
+	}
+}
+
+// VisitStrMapSlice 根据 path 访问 map[interface{}]interface{} 获取 []map[string]interface{} 值
+func VisitStrMapSlice(m map[interface{}]interface{}, paths ...interface{}) ([]map[string]interface{}, error) {
+	if value, err := VisitValue(m, paths); err == nil {
+		if result, ok := value.([]map[string]interface{}); ok {
+			return result, nil
+		}
+	}
+
+	if slice, err := visitSliceValue(m, paths); err == nil {
+		result := make([]map[string]interface{}, 0)
+		for _, s := range slice {
+			if dic, ok := s.(map[string]interface{}); ok {
+				result = append(result, dic)
+			}
+		}
+		return result, nil
+	} else {
+		return nil, err
+	}
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -320,5 +320,583 @@ func TestAsStrMapFail(t *testing.T) {
 		fmt.Println(result)
 		t.Fail()
 	}
+}
 
+func TestVisitWithNil(t *testing.T) {
+	if _, err := Visit(nil); err == nil {
+		t.Fail()
+	}
+}
+
+func TestWithNilPath(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+	if r, err := Visit(m); err != nil {
+		t.Fail()
+	} else {
+		if v, ok := r.(map[interface{}]interface{}); !ok {
+			t.Fail()
+		} else {
+			if v["a"] != 1 {
+				t.Fail()
+			}
+		}
+	}
+}
+
+func TestVisit(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+	if r, err := Visit(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitStr(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": "b",
+	}
+
+	if r, err := VisitStr(m, "a"); err != nil {
+		t.Error("Get str value failed.")
+	} else {
+		if r != "b" {
+			t.Error("Get str value incorrect.")
+		}
+	}
+
+}
+
+func TestVisitStrFail(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+
+	if _, err := VisitStr(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitStrNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": "b",
+	}
+
+	if _, err := VisitStr(m, "c"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitInt(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+
+	if r, err := VisitInt(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitIntFail(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": "b",
+	}
+
+	if _, err := VisitInt(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitIntNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+
+	if _, err := VisitInt(m, "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitBool(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": true,
+	}
+
+	if r, err := VisitBool(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r != true {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitBoolFail(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": "b",
+	}
+
+	if _, err := VisitBool(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitBoolNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": true,
+	}
+
+	if _, err := VisitBool(m, "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitSlice(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []interface{}{
+			1, 2,
+		},
+	}
+
+	if r, err := VisitSlice(m, "a"); err != nil {
+		t.Error("Get slice failed")
+	} else {
+		if r[0] != 1 || r[1] != 2 {
+			t.Error("Slice value incorrect")
+		}
+	}
+}
+
+func TestVisitSliceIncorrectType(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []int{
+			1, 2,
+		},
+	}
+
+	if _, err := VisitSlice(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitSliceNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []interface{}{
+			1, 2,
+		},
+	}
+
+	if _, err := VisitSlice(m, "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFromSlice(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []interface{}{
+			1, 2,
+		},
+	}
+
+	if r, err := VisitInt(m, "a", 0); err != nil {
+		t.Fail()
+	} else {
+		if r != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitFromSliceOOIndex(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []interface{}{
+			1, 2,
+		},
+	}
+
+	if _, err := VisitInt(m, "a", 2); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFromSliceNonIntIndex(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []interface{}{
+			1, 2,
+		},
+	}
+
+	if _, err := VisitInt(m, "a", "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitStrSlice(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []string{
+			"c", "d",
+		},
+	}
+
+	if r, err := VisitStrSlice(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r[0] != "c" || r[1] != "d" {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitStrSliceIncorrectType(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []int{
+			1, 2,
+		},
+	}
+
+	if _, err := VisitStrSlice(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitStrSliceNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []int{
+			1, 2,
+		},
+	}
+
+	if _, err := VisitStrSlice(m, "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFromStrSlice(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []string{
+			"b", "c",
+		},
+	}
+
+	if r, err := VisitStr(m, "a", 0); err != nil {
+		t.Fail()
+	} else {
+		if r != "b" {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitFromStrSliceOOIndex(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []string{
+			"b", "c",
+		},
+	}
+
+	if _, err := VisitStr(m, "a", 2); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFromStrSliceNonIntIndex(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []string{
+			"b", "c",
+		},
+	}
+
+	if _, err := VisitStr(m, "a", "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitMap(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": map[interface{}]interface{}{
+			"b": 1,
+		},
+	}
+
+	if r, err := VisitMap(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r["b"] != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitMapIncorrectType(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": map[interface{}]int{
+			"b": 1,
+		},
+	}
+
+	if _, err := VisitMap(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitMapNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": map[interface{}]interface{}{
+			"b": 1,
+		},
+	}
+
+	if _, err := VisitMap(m, "c"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFromMap(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+
+	if r, err := VisitInt(m, "a"); err != nil {
+		t.Fail()
+	} else if r != 1 {
+		t.Fail()
+	}
+}
+
+func TestVisitFromMapNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+
+	if _, err := VisitInt(m, "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitStrMap(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": map[string]interface{}{
+			"b": 1,
+		},
+	}
+
+	if r, err := VisitStrMap(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r["b"] != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitStrMapIncorrectType(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": map[interface{}]int{
+			"b": 1,
+		},
+	}
+
+	if _, err := VisitStrMap(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitStrMapNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": map[string]interface{}{
+			"b": 1,
+		},
+	}
+
+	if _, err := VisitStrMap(m, "a", "c"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFromStrMap(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": map[string]interface{}{
+			"b": 1,
+		},
+	}
+
+	if r, err := VisitInt(m, "a", "b"); err != nil {
+		t.Fail()
+	} else if r != 1 {
+		t.Fail()
+	}
+}
+
+func TestVisitFromStrMapNonExist(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": map[string]interface{}{
+			"b": 1,
+		},
+	}
+
+	if _, err := VisitInt(m, "a", "c"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitMapSlice(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []interface{}{
+			map[interface{}]interface{}{"b": 1},
+			map[interface{}]interface{}{"c": 2},
+		},
+	}
+
+	if r, err := VisitMapSlice(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r[0]["b"] != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitMapSlice2(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []map[interface{}]interface{}{
+			map[interface{}]interface{}{"b": 1},
+			map[interface{}]interface{}{"c": 2},
+		},
+	}
+	if r, err := VisitMapSlice(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r[0]["b"] != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitMapSliceFail(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+	if _, err := VisitMapSlice(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFromMapSlice(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []map[interface{}]interface{}{
+			map[interface{}]interface{}{
+				"b": 1,
+			},
+		},
+	}
+
+	if r, err := VisitInt(m, "a", 0, "b"); err != nil {
+		t.Fail()
+	} else if r != 1 {
+		t.Fail()
+	}
+}
+
+func TestVisitFromMapSliceFail(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []map[interface{}]interface{}{
+			map[interface{}]interface{}{
+				"b": 1,
+			},
+		},
+	}
+
+	if _, err := VisitInt(m, "a", 1, "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitStrMapSlice(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []interface{}{
+			map[string]interface{}{"b": 1},
+			map[string]interface{}{"c": 2},
+		},
+	}
+
+	if r, err := VisitStrMapSlice(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r[0]["b"] != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitStrMapSlice2(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []map[string]interface{}{
+			map[string]interface{}{"b": 1},
+			map[string]interface{}{"c": 2},
+		},
+	}
+
+	if r, err := VisitStrMapSlice(m, "a"); err != nil {
+		t.Fail()
+	} else {
+		if r[0]["b"] != 1 {
+			t.Fail()
+		}
+	}
+}
+
+func TestVisitStrMapSliceFail(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": 1,
+	}
+
+	if _, err := VisitStrMapSlice(m, "a"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFromStrMapSlice(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []map[string]interface{}{
+			map[string]interface{}{"b": 1},
+		},
+	}
+
+	if r, err := VisitInt(m, "a", 0, "b"); err != nil {
+		t.Fail()
+	} else if r != 1 {
+		t.Fail()
+	}
+}
+
+func TestVisitFromStrMapSliceFali(t *testing.T) {
+	m := map[interface{}]interface{}{
+		"a": []map[string]interface{}{
+			map[string]interface{}{"b": 1},
+		},
+	}
+
+	if _, err := VisitInt(m, "a", 1, "b"); err == nil {
+		t.Fail()
+	}
+}
+
+func TestVisitFail(t *testing.T) {
+	type x struct{}
+	m := map[interface{}]interface{}{
+		"a": x{},
+	}
+	if _, err := Visit(m, "a", "x"); err == nil {
+		t.Fail()
+	}
 }
